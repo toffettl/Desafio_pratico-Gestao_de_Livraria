b14b50ce29c392bb79ea8a9d6285e781e7dd90aa
toffettl <toffetti0824@gmail.com>
2025-04-01 16:52:14 -0300

    Exercicio finalizado

---
+++ Desafio prático - Gestão de Livraria/Controllers/BookController.cs
@@ -0,0 +1,66 @@
+﻿using Desafio_prático___Gestão_de_Livraria.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Desafio_prático___Gestão_de_Livraria.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : ControllerBase
+{
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var response = new Book
+        {
+            Id = id,
+            Title = "Qualquer",
+            Author = "Felipe",
+            AmountStock = 2,
+            Gender = "Terror",
+            Price = 23.30M
+        };
+
+        return Ok(response);
+    }
+
+    [HttpPost]
+    public IActionResult Create(Book book)
+    {
+        var response = new Book
+        {
+            Id = book.Id,
+            Title = book.Title,
+            Author = book.Author,
+            AmountStock = book.AmountStock,
+            Gender = book.Gender,
+            Price = book.Price
+        };
+
+        return Created(string.Empty, response);
+    }
+
+    [HttpPut]
+    public IActionResult Update(Book book)
+    {
+        return NoContent();
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int id)
+    {
+        return NoContent();
+    }
+
+    [HttpGet]
+    public IActionResult GetAll(int id)
+    {
+        var response = new List<Book>()
+        {
+            new Book { Id = 1 },
+            new Book { Id = 2 },
+            new Book { Id = 3 },
+        };
+
+        return Ok(response);
+    }
+}
---
+++ Desafio prático - Gestão de Livraria/Entites/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Desafio_prático___Gestão_de_Livraria.Entites;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Gender { get; set; }
+    public decimal Price { get; set; }
+    public int AmountStock { get; set; }
+}
